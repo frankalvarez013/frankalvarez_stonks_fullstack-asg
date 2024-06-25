a0389d9a602f93ba05e7a38f9ce0991fa1e69ee0
Frank Alvarez <frankalvarez013@gmail.com>
2024-06-25 16:12:25 -0700

    verify -7

--- app/[lng]/(home)/SetUp/page.tsx
+++ app/[lng]/(home)/SetUp/page.tsx
@@ -1,14 +1,14 @@
-import { createClient } from "@/utils/supabase/server";
+import { supabaseServer } from "@/utils/supabase/server2";
 import InitUser from "@/store/initUser";
 import SetUpForm from "./setUpForm";
 export default async function SetUp() {
-  const supabase = createClient();
-  const { data, error } = await supabase.auth.getUser();
-  console.log("AUTH - GeT USER", data, error);
+  const supabase = supabaseServer();
+  const { data, error } = await supabase.auth.getSession();
+  console.log("AUTH - GeT USER", data.session!.user, error);
   return (
     <div className="min-h-screen w-full flex items-center justify-center ">
       <SetUpForm></SetUpForm>
-      <InitUser user={data.session?.user || []}></InitUser>
+      <InitUser user={data.session?.user}></InitUser>
     </div>
   );
 }
