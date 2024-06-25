c9d42a6db84eced582ae8f233a7a5bc14bd1bcce
Frank Alvarez <frankalvarez013@gmail.com>
2024-06-25 00:02:28 -0700

    push

--- app/api/stream/route.ts
+++ app/api/stream/route.ts
@@ -1,30 +1,33 @@
-import { createClient } from "@/utils/supabase/server";
+import { createClient } from "@/utils/supabase/client";
 import { NextResponse } from "next/server";
 
 const supabase = createClient();
 
-async function notifyUsers() {}
-export async function POST(request, res) {
+export async function POST(request) {
   try {
     const channel = supabase.channel("streamer_channel");
-    channel.subscribe(async (status) => {
-      if (status === "SUBSCRIBED") {
-        const sendStatus = await channel.send({
-          type: "broadcast",
-          event: "streamer_online",
-          payload: { message: "Streamer is online" },
-        });
+    const { error: subscribeError } = await channel.subscribe();
 
-        if (sendStatus !== "ok") {
-          console.error("Error notifying users");
-          return NextResponse.json({ message: "failed" }, { status: 500 });
-        }
+    if (subscribeError) {
+      console.error("Subscription error", subscribeError);
+      return NextResponse.json({ message: "failed" }, { status: 500 });
+    }
 
-        return NextResponse.json({ message: "yay" }, { status: 201 });
-      }
+    const { error: sendError } = await channel.send({
+      type: "broadcast",
+      event: "streamer_online",
+      payload: { message: "Streamer is online" },
     });
+
+    if (sendError) {
+      console.error("Error notifying users", sendError);
+      return NextResponse.json({ message: "failed" }, { status: 500 });
+    }
+
+    return NextResponse.json({ message: "yay" }, { status: 201 });
   } catch (error) {
     console.error("Server error", error);
+    return NextResponse.json({ message: "Server error" }, { status: 500 });
   }
-  return NextResponse.json({ message: "yay" }, { status: 201 });
+  return NextResponse.json({ message: "Server error" }, { status: 500 });
 }
