357bc3d938adb68a256b6149785f854f459d9a95
Frank Alvarez <frankalvarez013@gmail.com>
2024-06-28 12:27:47 -0700

    fixed login issue

--- components/AuthButton.tsx
+++ components/AuthButton.tsx
@@ -19,7 +19,9 @@ export default async function AuthButton() {
 
   return user ? (
     <div className="flex items-center gap-4">
-      <div className=" sm:hidden md:visible">Hey, {user.email}!</div>
+      <div className="hidden sm:block md:block lg:block">
+        Hey, {user.email}!
+      </div>
       <form action={signOut}>
         <button className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover">
           Logout
