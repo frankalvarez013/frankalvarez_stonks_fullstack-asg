f6c51f6fcd2eb9efbe4815353d753a0205319cab
Frank Alvarez <frankalvarez013@gmail.com>
2024-06-28 12:43:47 -0700

    logout issue

--- components/AuthButton.tsx
+++ components/AuthButton.tsx
@@ -14,7 +14,7 @@ export default async function AuthButton() {
 
     const supabase = createClient();
     await supabase.auth.signOut();
-    return redirect("/");
+    location.reload();
   };
 
   return user ? (
